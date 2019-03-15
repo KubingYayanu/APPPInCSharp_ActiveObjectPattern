2abf26184c90c3c1a10eccc0a700717f416fd4d0
Kubing <t6m8@hotmail.com>
2019-03-15 14:00:33 +0800

    refactoring DelayedTyper

---
+++ APPPInCSharp_ActiveObjectPattern.Console/DelayedTyper.cs
@@ -0,0 +1,30 @@
+﻿namespace APPPInCSharp_ActiveObjectPattern.Console
+{
+    public class DelayedTyper : Command
+    {
+        private long itsDelay;
+        private char itsChar;
+        public static bool stop = false;
+        public static ActiveObjectEngine engine = new ActiveObjectEngine();
+
+        public DelayedTyper(long delay, char c)
+        {
+            itsDelay = delay;
+            itsChar = c;
+        }
+
+        public void Execute()
+        {
+            System.Console.Write(itsChar);
+            if (!stop)
+            {
+                DelayAndRepeat();
+            }
+        }
+
+        private void DelayAndRepeat()
+        {
+            engine.AddCommand(new SleepCommand(itsDelay, engine, this));
+        }
+    }
+}
---
+++ APPPInCSharp_ActiveObjectPattern.Console/StopCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace APPPInCSharp_ActiveObjectPattern.Console
+{
+    public class StopCommand : Command
+    {
+        public void Execute()
+        {
+            DelayedTyper.stop = true;
+        }
+    }
+}
